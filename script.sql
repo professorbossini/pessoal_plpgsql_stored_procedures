a71d0c3b9b34bfae6bcd50935d5ddb2c2370914d
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-02 16:23:50 -0300

    Exemplo com VARIADIC

--- script.sql
+++ script.sql
@@ -1,3 +1,34 @@
+CREATE OR REPLACE PROCEDURE sp_calcula_media ( VARIADIC  valores INT [])
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	media NUMERIC(10, 2) := 0;
+	valor INT;
+	
+BEGIN
+	FOREACH valor IN ARRAY valores LOOP
+		media := media + valor;	
+	END LOOP;
+	--array_length calcula o número de elementos no array. O segundo parâmetro é o número de dimensões dele
+	RAISE NOTICE 'A média é %', media / array_length(valores, 1);
+END;
+$$
+
+-- 1 parâmetro
+CALL sp_calcula_media(1);
+
+-- 2 parâmetros
+CALL sp_calcula_media(1, 2);
+
+-- 6 parâmetros
+CALL sp_calcula_media(1, 2, 5, 6, 1, 8);
+
+-- não funciona
+CALL sp_calcula_media (ARRAY[1, 2]);
+
+
+
+
 DROP PROCEDURE IF EXISTS sp_acha_maior;
 -- criando
 CREATE OR REPLACE PROCEDURE sp_acha_maior (INOUT valor1 INT, IN valor2 INT)
